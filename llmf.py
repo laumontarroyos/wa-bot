4441272ee13513362b301ecda0e7246a56dcd1c9
Laureano Montarroyos <laumontarroyos@gmail.com>
2020-07-27 09:10:29 -0300

    7

--- llmf.py
+++ llmf.py
@@ -56,6 +56,25 @@ def respond():
     return 'ok'
 
 
+@app.route('/1340265163:AAFcWrGSFvksnifXziCxYT9sGVjRbuMEwfI', methods=['POST'])
+def responde():
+    print("iniciou2...")
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # get the chat_id to be able to respond to the same user
+    chat_id = update.message.chat.id
+    # get the message id to be able to reply to this specific message
+    msg_id = update.message.message_id
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    # here we call our super AI
+    response = "Uma resposta simples2..."
+    # now just send the message back
+    # notice how we specify the chat and the msg we reply to
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    return 'ok'
+
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     # we use the bot object to link the bot to our app which live
