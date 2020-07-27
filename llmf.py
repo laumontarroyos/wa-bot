3af97be5ef5b6a17fe4f4788f0eb24ad4b204791
Laureano Montarroyos <laumontarroyos@gmail.com>
2020-07-27 10:11:16 -0300

    8

--- llmf.py
+++ llmf.py
@@ -3,6 +3,9 @@ import requests
 import json
 import logging
 import telegram
+#import telegram.bot
+#import telegram.ext.
+#from telegram.ext import Updater
 #from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
@@ -27,53 +30,35 @@ import telegram
 global bot
 global TOKEN
 TOKEN = "1340265163:AAFcWrGSFvksnifXziCxYT9sGVjRbuMEwfI"
-bot = telegram.Bot(token=TOKEN)
+bot1 = telegram.Bot(token=TOKEN)
 URL = "https://llmf-bot.herokuapp.com"
-#URL = "http://localhost:5000"
-#URL = "https://893e4c2528a1.ngrok.io"
-#URL = "https://bb61b29222fa.ngrok.io"
+
 
 app = Flask(__name__)
 
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    print("iniciou...")
+    
+    update = telegram.Update.de_json(data= request.get_json(force=True), bot1)
+    print('I am still alive.')
     # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    #update = telegram.Update.de_json(request.get_json(force=True), bot)
     # get the chat_id to be able to respond to the same user
     chat_id = update.message.chat.id
     # get the message id to be able to reply to this specific message
     msg_id = update.message.message_id
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
-    # here we call our super AI
+    
     response = "Uma resposta simples..."
     # now just send the message back
     # notice how we specify the chat and the msg we reply to
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    bot1.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    
     return 'ok'
 
 
-@app.route('/1340265163:AAFcWrGSFvksnifXziCxYT9sGVjRbuMEwfI', methods=['POST'])
-def responde():
-    print("iniciou2...")
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    # get the chat_id to be able to respond to the same user
-    chat_id = update.message.chat.id
-    # get the message id to be able to reply to this specific message
-    msg_id = update.message.message_id
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
-    # here we call our super AI
-    response = "Uma resposta simples2..."
-    # now just send the message back
-    # notice how we specify the chat and the msg we reply to
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-    return 'ok'
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
